509622d86bdb0b2da7ee09289214ed99c95a2c02
Tomas <1403466+tomasvanrijsse@users.noreply.github.com>
2019-12-06 17:20:33 +0100

    Use data table for task list

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>
-    Tasklist
-  </div>
+  <v-data-table
+    :headers="headers"
+    :items="tasks"
+  >
+    <template v-slot:item.name="{ item }">
+      <v-text-field
+        v-model="item.name"
+        placeholder="Task name"
+      />
+    </template>
+
+    <template v-slot:item.low="{ item }">
+      <v-text-field v-model="item.low" class="number" />
+    </template>
+
+    <template v-slot:item.target="{ item }">
+      <v-text-field v-model="item.target" class="number" />
+    </template>
+
+    <template v-slot:item.high="{ item }">
+      <v-text-field v-model="item.high" class="number" />
+    </template>
+
+    <template v-slot:item.action="{ item }">
+      <v-icon @click="deleteItem(item)" small>
+        mdi-delete
+      </v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
-  name: 'TaskList'
+  name: 'TaskList',
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Task description',
+          align: 'left',
+          sortable: true,
+          value: 'name'
+        },
+        { text: 'Low', value: 'low' },
+        { text: 'Target', value: 'target' },
+        { text: 'High', value: 'high' },
+        { text: 'Actions', value: 'action', sortable: false }
+      ],
+      tasks: [{
+        name: 'Task 1',
+        low: 1,
+        target: 4,
+        high: 10
+      }]
+    }
+  },
+  methods: {
+    deleteItem (item) {
+      const index = this.tasks.indexOf(item)
+      confirm('Are you sure you want to delete this task?') && this.tasks.splice(index, 1)
+    }
+  }
 }
 </script>
+
+<style>
+  .number {
+    max-width: 50px;
+    text-align: center;
+  }
+</style>
