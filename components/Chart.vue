d65e166d649330e916f722a7b0c74cfc286064b4
Tomas van Rijsse <1403466+tomasvanrijsse@users.noreply.github.com>
2020-01-05 21:30:57 +0100

    calculate sample outcomes

--- components/Chart.vue
+++ components/Chart.vue
@@ -5,10 +5,38 @@
 </template>
 
 <script>
+const d3 = require('d3-random')
+
 export default {
   name: 'Chart',
-  props: {
-    tasks: Array
+  computed: {
+    chartData () {
+      const tasks = this.$store.state.tasks.list
+
+      const samples = []
+
+      for (let i = 0; i < 1000; i++) {
+        let sum = 0
+
+        tasks.forEach(function (task, index) {
+          const random = d3.randomNormal(0.5, 0.15)()
+          let value = 0
+
+          if (random < 0.5) {
+            value = (random * 2) * (task.target - task.low)
+          } else {
+            value = (task.target - task.low)
+            value += ((random % 0.5) * 2) * (task.high - task.target)
+          }
+
+          const score = task.low + value
+          sum += score
+        })
+
+        samples.push(sum)
+      }
+      return samples
+    }
   }
 }
 </script>
